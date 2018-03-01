b0f51a04fa2e359e5d5c347362dd7627a6937423
NikaTikhonovets <nika.tikhonovets@gmail.com>
2018-03-01 13:10:07 +0300

    Add filtering (#26)
    * Create a filtering by name
    
    * Edit table columns
    
    * Fix conflicts
    
    * Add classes for filtering

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,28 @@
       $('.navigation').toggleClass('show');
     });
 
+
+    $('.filterable .filters input').keyup(function (e) {
+
+      var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+
+      var $filteredRows = $rows.filter(function () {
+        var value = $(this).find('td').eq(column).text().toLowerCase();
+        return value.indexOf(inputContent) === -1;
+      });
+      $table.find('tbody .no-result').remove();
+
+      $rows.show();
+      $filteredRows.hide();
+
+      if ($filteredRows.length === $rows.length) {
+        $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="' + $table.find('.filters th').length + '">No result found</td></tr>'));
+      }
+    });
   });
 })(jQuery);
