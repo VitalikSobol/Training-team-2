a516b79a44e4e88cbe4aca8f539faba8f0c5dfa2
Unknown <nika.tikhonovets@gmail.com>
2018-04-15 12:54:06 +0300

    Change profile page (Angular 5)

--- client/src/app/component/app.module.ts
+++ client/src/app/component/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DropdowsStatusComponent } from './dropdown-status/dropdown-status.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotificationBlockComponent } from './notification-block/notification-block.component';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { ModalModule } from 'ngx-bootstrap';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    DropdowsStatusComponent,
+    MenuComponent,
+    NotificationBlockComponent,
+    SearchInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/component/profile/user.ts
@@ -0,0 +1,21 @@
+class experience {
+  period: string;
+  position: string;
+  location: string;
+  company: string;
+  description: string;
+}
+
+export class User {
+  name: string;
+  lastName: string;
+  position: string;
+  salary: number;
+  address: string;
+  phone: string;
+  status: string;
+  email: string;
+  skills: string[];
+  experiences: experience[];
+}
+
