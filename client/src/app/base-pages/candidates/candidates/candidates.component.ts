d9f646c80ae3f3e4894a429715f926790e21514d
VitalikSobol <vitaliysobol93@gmail.com>
2018-04-29 11:35:09 +0300

    Added filtering in Candidate table

--- client/src/app/base-pages/candidates/candidates/candidates.component.ts
+++ client/src/app/base-pages/candidates/candidates/candidates.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import {Candidate} from './candidate';
 import {CandidateService} from '../../service/candidate/candidate.service';
+import {Filter} from "../../service/candidate/filter";
 
 @Component({
   moduleId: module.id,
@@ -13,39 +14,23 @@ import {CandidateService} from '../../service/candidate/candidate.service';
 export class CandidatesComponent implements OnInit {
 
   candidates: Candidate[] = [];
-    /*{
-      photo: 'http://via.placeholder.com/500x350/66bfff',
-      name: 'Ivan',
-      email: 'ivanov@gmail.com',
-      position: 'Java Developer',
-      date: '2018-03-24',
-      status: 'New'
-    },
-    {
-      photo: 'http://via.placeholder.com/500x350/66bfff',
-      name: 'Ward',
-      email: 'ward@gmail.com',
-      position: '.Net Developer',
-      date: '2018-03-02',
-      status: 'Accepted for Interview'
-    },
-    {
-      photo: 'http://via.placeholder.com/500x350/66bfff',
-      name: 'Augustine',
-      email: 'aug@gmail.com',
-      position: 'JavaScript Developer',
-      date: '2018-03-03',
-      status: 'CV-Rejected'
-    },
-    {
-      photo: 'http://via.placeholder.com/500x350/66bfff',
-      name: 'Colombina',
-      email: 'gae@gmail.com',
-      position: 'Java Developer',
-      date: '2018-03-03',
-      status: 'CV-Accepted'
-    }
-  ];*/
+  filter: Filter = {
+    name:'',
+    position:'',
+    date:'',
+    status:'',
+    email:''
+  }
+
+  pagination = {
+    rows:10,
+    begin:0,
+    page:1
+  }
+
+  //total:number;
+  //range:string;
+  //currentRows: number = 10;
 
   constructor( private candidateService: CandidateService) {
   }
@@ -55,10 +40,21 @@ export class CandidatesComponent implements OnInit {
   }
 
   getCandidates(){
-      this.candidateService.getCandidates().subscribe((data: any) => {
+      this.candidateService.getCandidates(this.filter, this.pagination).subscribe((data: any) => {
       console.log(data);
       this.candidates = data.data;
+      //this.total = data.total;
+      //this.range = data.range;
      });
     }
 
+  filtering(id,filterValue){
+    this.filter[id] = filterValue;
+    this.getCandidates();
+  }
+
+  /*changeRowsNumber($event){
+    this.currentRows = $event;
+    console.log($event);
+  }*/
 }
